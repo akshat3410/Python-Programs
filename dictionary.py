3195e70c9f44e1f05c1f9af55d3dbb110b012f6e
akshat3410 <69171692+akshat3410@users.noreply.github.com>
2022-03-29 14:26:34 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+dict_1 = {
+"Awedde":"It means to overcome anger.", 
+"Awel":"Any of various types of hook or hook instrument.",
+"Awesomesouce":"Extremely Good"} 
+
+updater = input("Which word you want to update:")
+meaning = input("Enter the meaning.")
+dict_1[updater] =  meaning
+
+print(dict_1)
+print("Your word added successfully.")
+
